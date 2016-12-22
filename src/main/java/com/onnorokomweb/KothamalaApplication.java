ca9ce7c241a0c5af5eaf96fb044d923ba48c373d
ronniegnr <gnr.ronnie@gmail.com>
2016-12-22 10:33:12 +0600

    slf4j logger initialize

--- src/main/java/com/onnorokomweb/KothamalaApplication.java
+++ src/main/java/com/onnorokomweb/KothamalaApplication.java
@@ -1,5 +1,7 @@
 package com.onnorokomweb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
@@ -9,10 +11,13 @@ import javax.inject.Inject;
 @SpringBootApplication
 public class KothamalaApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(KothamalaApplication.class);
+
     @Inject
     private Environment environment;
 
-	public static void main(String[] args) {
-		Environment environment = SpringApplication.run(KothamalaApplication.class, args).getEnvironment();
+    public static void main(String[] args) {
+        Environment environment = SpringApplication.run(KothamalaApplication.class, args).getEnvironment();
+        logger.info("log testing");
     }
 }
