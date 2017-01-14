2a27ee84411d52f39e442d6e429fcd029b37705e
Arif Shakil <Arif Shakil>
2017-01-14 21:56:26 +0600

    initial commit for tag table

---
+++ src/main/java/com/ronniegnr/kothamala/domain/Tag.java
@@ -0,0 +1,69 @@
+package com.ronniegnr.kothamala.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Email;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tag")
+public class Tag extends AbstractAuditingEntity implements Serializable {
+
+    private static final long serialVersionUID = 1;
+
+    public enum Status { ACTIVE, INACTIVE, BANNED }
+
+    private long id;
+    private String name;
+    private Status status;
+
+    private Set<Authority> authorities = new HashSet<>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @NotNull
+    @Size(max = 45)
+    @Column(name = "name", nullable = false, length = 50)
+    public String getFirstName() {
+        return name;
+    }
+
+    public void setFirstName(String firstName) {
+        this.name = firstName;
+    }
+    
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name +
+                ", status=" + status +
+                '}';
+    }
+}
