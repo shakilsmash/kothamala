a583de02b8c03a1b7e960c1f4124e0fd4a298724
Arif Shakil <Arif Shakil>
2017-01-15 11:09:04 +0600

    removed unused variables

--- src/main/java/com/ronniegnr/kothamala/domain/Vote.java
+++ src/main/java/com/ronniegnr/kothamala/domain/Vote.java
@@ -2,7 +2,6 @@ package com.ronniegnr.kothamala.domain;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity
